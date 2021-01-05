4ffc27fcc5d88c58e57c58fa4c792d02dc003027
Ruzhytska <anastasiya.ruzhytska@accenture.com>
2021-01-05 07:52:13 +0100

    error corregido

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import {Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/home';
 
 const Routes  = () => {
+    return(
     <BrowserRouter>
     <Switch>
         <Route path="/" component={Home}/>
     </Switch>
     
     </BrowserRouter>
+    )
 }
 
 export default Routes;
